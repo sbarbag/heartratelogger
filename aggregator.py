0578955af749987d837707f80ca17470eee873bc
Stephen Barbagallo <stephenbarbagallo.sb@gmail.com>
2025-03-04 18:32:35 -0800

    Adding csv export functionality and fixing bug associated with activities without heart rate data

--- aggregator.py
+++ aggregator.py
@@ -1,18 +1,20 @@
 from stravalib import Client
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import pandas as pd
 
-MY_TOKEN = 'Token from token_generation.py here'
+MY_TOKEN = 'Your Token Here'
 
 client = Client(access_token=MY_TOKEN)
 
 print("Getting Activities")
-activities = client.get_activities(after="2025-01-01", limit=3)  
+activities = client.get_activities(after="2025-01-28", limit=2)  
 
 act_list =[]
 dates =[]
 descriptions = []
 act_types = []
+
+
 
 for activity in activities:
     act_list.append(activity.id)
@@ -29,16 +31,27 @@ zones = {"0": [],
 
 #Pull heart rate data from activitiy zones and organize into a dictionary
 print("Pulling and organizing zone data")
+k = 1
 for i in act_list:
-    get_hr_zones = client.get_activity_zones(i)[0].distribution_buckets #0 for HR, 1 for Power
-    for j in range(0,5):
-        zone_dur = get_hr_zones[j].time #returns time in zone in seconds 
-        if zone_dur is None:
-            zone_dur = 0
-        zones[str(j)].append(str(timedelta(seconds = zone_dur)))
-    print("...")
+    try:
+        get_hr_zones = client.get_activity_zones(i)[0].distribution_buckets #0 for HR, 1 for Power
+        for j in range(0,5):
+            zone_dur = get_hr_zones[j].time #returns time in zone in seconds 
+            if zone_dur is None:
+                zone_dur = 0
+            zones[str(j)].append(str(timedelta(seconds = zone_dur)))
+        print("Pulled data for activity " +str(k) +"...")
+        k = k + 1
+    except IndexError:
+        for j in range(0,5):
+            zones[str(j)].append(str(timedelta(seconds = 0)))
+        print("No Heart Rate Data found in Activity "+ str(k))
+        k = k + 1
 
-        
+#print(dates)
+#print(descriptions)
+#print(act_types)
+#print(zones)        
     
 df = pd.DataFrame({"date": dates,
                    "description": descriptions,
@@ -48,7 +61,12 @@ df = pd.DataFrame({"date": dates,
                    "z3": zones["2"],
                    "z4": zones["3"],
                    "z5": zones["4"]},
-                  index=act_list
+                  #index=act_list
                   )
 
 print(df)
+
+#Outputs a csv named with today's date    
+log_name = (str(datetime.today().strftime('%Y-%m-%d'))+".csv")
+df.to_csv(log_name, index=False)
+
--- token_generation.py
+++ token_generation.py
@@ -3,6 +3,6 @@ from dotenv import dotenv_values
 import webbrowser
 from strava_auth import get_token
 
-your_code_here = '' #your code
+your_code_here = '324a685ea22ff187fb677f44a5353bf354a266af' #your code
 
 get_token(your_code_here)
